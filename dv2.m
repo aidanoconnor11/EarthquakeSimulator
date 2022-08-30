93668f80166eefc7e53ec564024db98c6940199f
aidanoconnor11 <110209949+aidanoconnor11@users.noreply.github.com>
2022-08-30 13:19:26 -0400

    Add files via upload

---
+++ dv2.m
@@ -0,0 +1,3 @@
+function dy=dv2(t,x2) 
+    dy=[x2(2);((-552.5*(4.4*sin(((2*pi)/2.5)*t))-Fsp-Fd))/552.5]
+end
---
+++ dydt.m
@@ -0,0 +1,7 @@
+
+
+
+function dy=dydt(t,x) 
+    dy=[x(2);-0*((x(1)^2)-1)*x(2)-x(1)]
+end
+
